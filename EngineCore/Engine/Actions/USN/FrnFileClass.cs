92ef15d89b47b453647a538d21247d39c99893b1
luoji <luojin765@hotmail.com>
2025-10-14 13:15:51 +0800

    update parentReferenceNumber when USN_REASON_RENAME_NEW_NAME

--- EngineCore/Engine/Actions/USN/FrnFileClass.cs
+++ EngineCore/Engine/Actions/USN/FrnFileClass.cs
@@ -117,6 +117,7 @@ namespace TDSNET.Engine.Actions.USN
                             {
                                 GetNACNNameAndIndex(f.Name, out var nacnName, out var index);
                                 frn.innerFileName = nacnName;
+                                frn.parentFileReferenceNumber = f.ParentFileReferenceNumber;
                                 frn.keyindex = index;
                             }
                             else
